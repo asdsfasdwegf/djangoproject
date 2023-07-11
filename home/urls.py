665de8fbd260c3be83e0f4d68b6b1d190286a895
Aa.y <a.a.y.king.chess@gmail.com>
2023-07-12 00:01:54 +0330

    add home directory

---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello/', views.say_hello),
+    path('', views.home),
+    path('father', views.father),
+    path('detail/<int:todo_id>/', views.detail, name='details')
+]
---
+++ home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Todo
+
+
+def home(request):
+    all = Todo.objects.all()
+    return render(request, 'home.html', {'todos': all})
+
+
+def father(request):
+    return render(request, 'name.html')
+
+
+def say_hello(request):
+    person = {'name': 'ali'}
+    return render(request, 'hello.html', context=person)
+
+
+def detail(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    return render(request, 'detail.html', {'todo': todo})
