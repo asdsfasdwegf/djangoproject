52b2b89c23beda8248dfedd265b61e6ddbf1ad7e
Aa.y <a.a.y.king.chess@gmail.com>
2023-07-15 01:25:36 +0330

    final

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def user_register(request):
     if request.method == 'POST':
@@ -31,3 +31,7 @@ def user_login(request):
     else:
         form = UserLoginForm()
     return render(request, 'login.html', {'form':form})
+def user_logout(request):
+    logout(request)
+    messages.success(request, 'loging out', 'success')
+    return redirect('home')
